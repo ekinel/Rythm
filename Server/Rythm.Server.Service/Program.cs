d0db786cfdb30d97ca4f1e970c88361dfee325ae
Elena <kiseleva.e.98@gmail.com>
2020-11-12 12:21:58 +0700

    Add project skeleton

---
+++ Server/Rythm.Server.Service/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rythm.Server.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
