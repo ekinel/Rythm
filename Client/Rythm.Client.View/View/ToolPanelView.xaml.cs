3934c09cd03e9a000391a29a23fed47e720addb0
ekinel <kiseleva.e.98@gmail.com>
2020-12-28 11:19:21 +0700

    Theme change added

--- Client/Rythm.Client.View/View/ToolPanelView.xaml.cs
+++ Client/Rythm.Client.View/View/ToolPanelView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+
 
 namespace Rythm.Client.View
 {
@@ -20,9 +22,41 @@ namespace Rythm.Client.View
     /// </summary>
     public partial class ToolPanelView : UserControl
     {
+        private bool lightTheme = true;
         public ToolPanelView()
         {
             InitializeComponent();
+
+            List<string> styles = new List<string> { "ButtonStyle", "ButtonStyleDark" };
+
+            themeButton.Click += ClickButton;
+        }
+
+		private void ClickButton(object sender, RoutedEventArgs e)
+		{
+            lightTheme = !lightTheme;
+			string _theme = string.Empty;
+
+			switch (lightTheme)
+			{
+                case true:
+                    _theme = "ButtonStyle";
+                    break;
+
+                case false:
+                    _theme = "ButtonStyleDark";
+                    break;
+            }
+
+            // определяем путь к файлу ресурсов
+            var uri = new Uri(@"../../Resources/" + _theme + ".xaml", UriKind.Relative);
+            // загружаем словарь ресурсов
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            // очищаем коллекцию ресурсов приложения
+            Application.Current.Resources.Clear();
+            // добавляем загруженный словарь ресурсов
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+
         }
     }
 }
